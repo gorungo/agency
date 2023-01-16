780a5750d9124a049100e1f9ad9952e9dc6e1c53
inkas <denis.g.petrov@gmail.com>
2023-01-17 09:33:41 +1000

    change border radius

--- components/ServicesNavCards.vue
+++ components/ServicesNavCards.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-6">
         <nuxt-link :to="localePath('/services/internet')">
-          <div class="card card-body text-center bg-light-panel bg-gr-2">
+          <div class="card card-body bottom-left-radius-5 text-center bg-light-panel bg-gr-2">
             <img src="/images/icon/services-internet.svg" height="200" alt="" />
             <h3>{{$t('services.internet.title')}}</h3>
           </div>
@@ -11,7 +11,7 @@
       </div>
       <div class="col-6">
         <nuxt-link :to="localePath('/services/video')">
-          <div class="card card-body bottom-left-radius-5 text-center bg-gr-1">
+          <div class="card card-body text-center bg-gr-1">
             <img src="/images/icon/services-video.svg" height="200" alt="" />
             <h3>{{$t('services.video.title')}}</h3>
           </div>
